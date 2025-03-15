37f89d31b50f406dedf189da0d76894cdf8b579e
Chris Sanford <ChrisSanfordBusiness@gmail.com>
2025-03-15 19:23:56 -0400

    Set CNAME

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   // Remove basePath and assetPrefix when using custom domain
   // basePath: process.env.NODE_ENV === 'production' ? '/Website' : '',
   // assetPrefix: process.env.NODE_ENV === 'production' ? '/Website/' : '',
+  // Make sure the CNAME file is copied to the output directory
+  async rewrites() {
+    return [];
+  },
 }
 
 module.exports = nextConfig 
